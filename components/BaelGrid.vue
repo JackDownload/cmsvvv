5d4862f7e1a038b280ac9f25a4f12742998cade3
JackDownload <kismocsy@gmail.com>
2021-06-20 10:29:23 +0200

    Revert "ds"
    This reverts commit 1897173b318e37d5b0722ad62741ba43e59a4ce6.

--- components/BaelGrid.vue
+++ components/BaelGrid.vue
@@ -20,35 +20,13 @@
       <div class="xs-p2 c-100 xs-flex xs-flex-align-center xs-flex-justify-center xs-text-center">
         <div v-if="posts.length < 1 && !busy">No Results.</div>
       </div>
-    </div>      <div class="xs-mt2 xs-p2 bcg-item">
-        <div class="item xs-block xs-full-height">
-          <lazy-featured-image
+    </div>
+                        <lazy-featured-image
             v-if="post.thumbnail"
             :title="post.title"
             :thumbnail="post.thumbnail"
           />
-
-          <h1 class="xs-py3 main-title">{{post.title}}</h1>
-          <div class="xs-mt-5 bold">
-            <ul class="list-unstyled xs-flex xs-flex-align-center">
-              <li class="xs-inline-block xs-mr1" v-if="post.category">
-                <div class="tag fill-gray-darker xs-border">
-                  <nuxt-link
-                    :to="`/category/${post.category.toLowerCase()}`"
-                    class="tag__link text-white"
-                  >{{post.category}}</nuxt-link>
-                </div>
-              </li>
-              <li class="xs-inline-block">{{ post.date }}</li>
-            </ul>
-          </div>
-          <div class="xs-py3 post-content text-gray-lighter">
-            <nuxt-content :document="post" />
-          </div>
-        </div>
-      </div>
   </div>
-  
 </template>
 
 <script>
